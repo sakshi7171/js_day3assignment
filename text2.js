b3cadb5c44cbb52642e3ee8aa3fb6904f7081ec6
sakshi7171 <61040929+sakshi7171@users.noreply.github.com>
2020-07-17 12:18:12 +0530

    Add files via upload

---
+++ text2.js
@@ -0,0 +1,17 @@
+console.log("Question 1 Day 3");
+var num = prompt("enter a number ");
+
+
+console.log(`number is ${num}`);
+
+if(num % 2 == 0)
+{
+    console.log ('Number is even!');
+} 
+else
+{
+   console.log('Number is odd!');
+}
+
+
+
